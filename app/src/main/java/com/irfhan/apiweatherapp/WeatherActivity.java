412ab676ae8d15afa8286809a71834cb069e57a2
Irfan Hanif <14.irfanh@gmail.com>
2023-05-16 09:57:42 +0700

    Created weather activity with its layout

---
+++ app/src/main/java/com/irfhan/apiweatherapp/WeatherActivity.java
@@ -0,0 +1,14 @@
+package com.irfhan.apiweatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_layout);
+    }
+}
