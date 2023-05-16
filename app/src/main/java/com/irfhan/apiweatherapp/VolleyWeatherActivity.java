6026142e1592be3981e5b1eb3856a7f6ca3948a6
Irfan Hanif <14.irfanh@gmail.com>
2023-05-16 11:07:30 +0700

    Added JSON request method inside class and Volley dependencies

--- app/src/main/java/com/irfhan/apiweatherapp/VolleyWeatherActivity.java
+++ app/src/main/java/com/irfhan/apiweatherapp/VolleyWeatherActivity.java
@@ -6,10 +6,21 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class VolleyWeatherActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class VolleyWeatherActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
 
     private TextView tvCondition, tvWindSpeed, tvTemperature, tvLocation, tvCoordinate;
     private ImageView ivWeatherIcon;
+
+    private String url = "https://api.open-meteo.com/v1/forecast?latitude=-7.98&longitude=112.63&daily=weathercode&current_weather=true&timezone=auto";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,31 @@ public class VolleyWeatherActivity extends AppCompatActivity {
         tvLocation = findViewById(R.id.tv_location);
         tvCondition = findViewById(R.id.tv_condition);
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, this, this);
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        try {
+            String longitude = response.getString("longitude");
+            String latitude = response.getString("latitude");
+            JSONObject currentWeather = response.getJSONObject("current_weather");
+            String temperature = currentWeather.getString("temperature");
+            String windSpeed = currentWeather.getString("windspeed");
+            int weatherCode = currentWeather.getInt("weathercode");
+
+            switch (weatherCode) {
+                case 0:
+                    tvCondition.setText("Clear sky");
+
+            }
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
 
     }
 }
