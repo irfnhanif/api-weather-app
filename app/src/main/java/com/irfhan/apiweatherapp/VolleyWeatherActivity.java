1bb00a34ac4d020978c3fb7a324cbb8e17f29971
Irfan Hanif <14.irfanh@gmail.com>
2023-05-18 18:11:03 +0700

    Finished the forecast part's logic

--- app/src/main/java/com/irfhan/apiweatherapp/VolleyWeatherActivity.java
+++ app/src/main/java/com/irfhan/apiweatherapp/VolleyWeatherActivity.java
@@ -13,13 +13,20 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class VolleyWeatherActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
 
     private TextView tvCondition, tvWindSpeed, tvTemperature, tvLocation, tvCoordinate;
-    private ImageView ivWeatherIcon;
+    private TextView tvDate1, tvDate2, tvDate3, tvDate4, tvDate5, tvDate6, tvDate7;
+    private ImageView ivWeatherIcon, ivWeatherDate1, ivWeatherDate2, ivWeatherDate3, ivWeatherDate4, ivWeatherDate5, ivWeatherDate6, ivWeatherDate7;
     private RequestQueue requestQueue;
 
     private String url = "https://api.open-meteo.com/v1/forecast?latitude=-7.98&longitude=112.63&daily=weathercode&current_weather=true&timezone=auto";
@@ -37,6 +44,22 @@ public class VolleyWeatherActivity extends AppCompatActivity implements Response
         tvCoordinate = findViewById(R.id.tv_coordinate);
         ivWeatherIcon = findViewById(R.id.iv_weather_icon);
 
+        tvDate1 = findViewById(R.id.tv_date1);
+        tvDate2 = findViewById(R.id.tv_date2);
+        tvDate3 = findViewById(R.id.tv_date3);
+        tvDate4 = findViewById(R.id.tv_date4);
+        tvDate5 = findViewById(R.id.tv_date5);
+        tvDate6 = findViewById(R.id.tv_date6);
+        tvDate7 = findViewById(R.id.tv_date7);
+
+        ivWeatherDate1 = findViewById(R.id.iv_weather1);
+        ivWeatherDate2 = findViewById(R.id.iv_weather2);
+        ivWeatherDate3 = findViewById(R.id.iv_weather3);
+        ivWeatherDate4 = findViewById(R.id.iv_weather4);
+        ivWeatherDate5 = findViewById(R.id.iv_weather5);
+        ivWeatherDate6 = findViewById(R.id.iv_weather6);
+        ivWeatherDate7 = findViewById(R.id.iv_weather7);
+
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, this, this);
 
         requestQueue = Volley.newRequestQueue(this);
@@ -53,6 +76,7 @@ public class VolleyWeatherActivity extends AppCompatActivity implements Response
             String windSpeed = currentWeather.getString("windspeed");
             String weatherCodeString = currentWeather.getString("weathercode");
 
+
             int weatherCode = Integer.parseInt(weatherCodeString);
             String coordinate = longitude + ", " + latitude;
 
@@ -62,6 +86,30 @@ public class VolleyWeatherActivity extends AppCompatActivity implements Response
             tvTemperature.setText(temperature);
             tvLocation.setText("Malang, East Java");
             tvCoordinate.setText(coordinate);
+
+            JSONObject daily = response.getJSONObject("daily");
+            JSONArray time = daily.getJSONArray("time");
+            JSONArray weatherCodes = daily.getJSONArray("weathercode");
+
+            List<String> timeArray = getTimeInArray(time);
+            List<Integer> weatherCodesArray = getWeatherCodesInArray(weatherCodes);
+
+            ivWeatherDate1.setImageResource(getWeatherIconResource(weatherCodesArray.get(0)));
+            ivWeatherDate2.setImageResource(getWeatherIconResource(weatherCodesArray.get(1)));
+            ivWeatherDate3.setImageResource(getWeatherIconResource(weatherCodesArray.get(2)));
+            ivWeatherDate4.setImageResource(getWeatherIconResource(weatherCodesArray.get(3)));
+            ivWeatherDate5.setImageResource(getWeatherIconResource(weatherCodesArray.get(4)));
+            ivWeatherDate6.setImageResource(getWeatherIconResource(weatherCodesArray.get(5)));
+            ivWeatherDate7.setImageResource(getWeatherIconResource(weatherCodesArray.get(6)));
+
+            tvDate1.setText(timeArray.get(0));
+            tvDate2.setText(timeArray.get(1));
+            tvDate3.setText(timeArray.get(2));
+            tvDate4.setText(timeArray.get(3));
+            tvDate5.setText(timeArray.get(4));
+            tvDate6.setText(timeArray.get(5));
+            tvDate7.setText(timeArray.get(6));
+
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
@@ -142,4 +190,32 @@ public class VolleyWeatherActivity extends AppCompatActivity implements Response
         }
     }
 
+
+    private List<String> getTimeInArray(JSONArray time) {
+        List<String> timeArray = new ArrayList<>();
+        for (int i = 0; i < time.length(); i++) {
+            try {
+                String day = time.getString(i);
+                timeArray.add(day);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return timeArray;
+    }
+
+    private List<Integer> getWeatherCodesInArray(JSONArray weatherCodes) {
+        List<Integer> weatherCodesArray = new ArrayList<>();
+        for (int i = 0; i < weatherCodes.length(); i++) {
+            try {
+                String weatherCode = weatherCodes.getString(i);
+                weatherCodesArray.add(Integer.parseInt(weatherCode));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return weatherCodesArray;
+    }
 }
